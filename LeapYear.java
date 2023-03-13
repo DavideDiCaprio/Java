3807a3f674c2e28ab73f75db2eb6f6f0bc713607
Giovanni Ciampi <giovanniciampi95@gmail.com>
2023-03-13 22:02:48 +0100

    add LeapYear assignment

---
+++ LeapYear.java
@@ -0,0 +1,6 @@
+/* Write a program that asks the user to input a number that represents a year,
+   then prints '{year} is a leap year!' or '{year} is NOT a leap year!' after
+   checking if the year is leap or not.
+   
+   Remember that a year is leap if it is a multiple of 4 and not a multiple of
+   100, or if it is a multiple of 400. */
