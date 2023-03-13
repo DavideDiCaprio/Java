9eba13eb4bc848cdc78bb7248aa4b91fcb909131
Giovanni Ciampi <giovanniciampi95@gmail.com>
2023-03-13 22:08:53 +0100

    add TemperatureConverter exercise

---
+++ TemperatureConverter.java
@@ -0,0 +1,10 @@
+/* Ask the user to input the current temperature, then ask the user to input 'F'
+   if it is measured in Fahreneit, 'C' if it is measured in Celsius.
+
+   1. Print a nicely formatted string 'Your temperature is (T) (U).' replacing
+      (T) with the temperature and (U) with the unit.
+
+   2. Convert the temperature to the other unit, then print the same message but
+      with the converted temperature and the appropriate unit.
+
+   *Note*: you can find online formulas to perform the conversions C <-> F. */
