94d199c97eb2a04ebedfc305d0e5d2a0927957ab
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-03-14 17:24:59 +0100

    add Greet class
    concat function doesn't work as well, I had a rage quit

--- Greet.java
+++ Greet.java
@@ -1,2 +1,20 @@
 /* Write a Java program that asks the user for their name and surname, then greets
    them with a string like 'Hi name surname, it is a pleasure to meet you!'.*/
+
+import java.util.Scanner;
+
+class Greet {
+    
+  public static void main(String[] args) {
+      
+      Scanner myScanner = new Scanner(System.in);
+      
+      System.out.println("Enter name!");
+      String userName = myScanner.nextLine();
+      System.out.println("Enter surname!");
+      String userSurname = myScanner.nextLine();
+
+      System.out.println("Hi " + userName.concat(userSurname) + ", it is a pleasure to meet you!");
+  }
+
+}
