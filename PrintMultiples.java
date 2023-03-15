48f404180e664e08a8a994a4a710cacf2647bfa3
Giovanni Ciampi <giovanniciampi95@gmail.com>
2023-03-15 20:31:19 +0100

    Add PrintMultiples Assignment

---
+++ PrintMultiples.java
@@ -0,0 +1,12 @@
+/* Write a java program that asks two numbers to the user, one will be the range limit and
+   another will be the divisor. Print all the numbers in the range ]divisor, limit] which
+   are integer multiples of divisor.
+   
+   Here is an example:
+   
+   limit = 20, divisor = 5.
+   output: 10, 15, 20
+   
+   limit = 30, divisor = 7.
+   output: 14, 21, 28
+ */
