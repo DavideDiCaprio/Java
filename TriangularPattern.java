1ee3d4e23b343275bcfd8a9331c33b501c5029eb
Giovanni Ciampi <giovanniciampi95@gmail.com>
2023-03-15 20:35:35 +0100

    Add TriangularPattern Assignment

---
+++ TriangularPattern.java
@@ -0,0 +1,20 @@
+/* Ask the user to input a number n, then print a triangle made of the '*' character with
+   n rows and n columns.
+
+   Examples:
+
+   n = 1
+   output = *
+
+   n = 2
+   output = *
+            **
+
+   n = 4
+   output = *
+            **
+            ***
+            ****
+
+*/
+   
